ecf6cdae6e44764aad0ae60f2744c7b52513c7d7
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-03-26 18:21:58 +0530

    Create  sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'goats-hub-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
