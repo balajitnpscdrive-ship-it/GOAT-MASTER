0d58c13036d526ffecaea9b80b2288224e7425ed
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-03-26 18:29:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,7 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-512.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap'
+  './icon-512.png'
 ];
 self.addEventListener('install', (event) => {
   event.waitUntil(
